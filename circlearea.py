aa8324485d39a9a6c7c3e0cf4c3ba581d92fd42d
Hameedah <adebanjohameedah7@gmail.com>
2020-03-25 16:05:08 +0100

    added python project files

---
+++ circlearea.py
@@ -0,0 +1,9 @@
+print('WELCOME!!!')
+
+#create variables or PI and allow input for Radius
+PI = 3.142
+Radius = float(input('To calculate the area of a circle, please enter the radius: '))
+Area = PI * Radius**2
+
+#Print out result
+print("The Area of the circle is: %.2f" % Area)
